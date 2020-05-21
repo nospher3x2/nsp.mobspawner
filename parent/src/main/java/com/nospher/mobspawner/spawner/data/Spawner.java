8970352cb86a004b84fcfa6002e75051d9c33094
oNospher <fealtyservers@gmail.com>
2020-05-21 18:09:26 -0300

    Update Spawner.java

--- parent/src/main/java/com/nospher/mobspawner/spawner/data/Spawner.java
+++ parent/src/main/java/com/nospher/mobspawner/spawner/data/Spawner.java
@@ -1,7 +1,21 @@
 package com.nospher.mobspawner.spawner.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+
 /**
  * @author oNospher
  **/
+@AllArgsConstructor
+@Getter @Setter
 public class Spawner {
+
+    private final Integer id;
+    private final EntityType entity;
+    private final Location location;
+    private Boolean enabled;
+
 }
